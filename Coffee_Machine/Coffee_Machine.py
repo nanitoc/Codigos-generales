6d50d030c48ae31fd6fb9253d0e9b21b3bbaecbe
nanitoc <86374201+nanitoc@users.noreply.github.com>
2021-07-26 16:26:51 -0400

    correccionesy optimizaciones

--- Coffee_Machine/Coffee_Machine.py
+++ Coffee_Machine/Coffee_Machine.py
@@ -1,9 +1,8 @@
 from art import Logo,MENU,resources
-
+import os
 # Global variables
-options=["espresso","latte","cappuccino"]
+options=["espresso","latte","cappuccino","report","off"]
 money=0
-
 
 #funcion para ver si hay con que preparar el cafe
 def check_resources(sel):
@@ -17,31 +16,30 @@ def check_resources(sel):
 #funcion para actualizar resources
 def updateresouces(sel2): 
     for x in MENU[sel2]["ingredients"]:
-        resources[x]=resources[x]-MENU[sel2]["ingredients"][x]     
+        resources[x]-=MENU[sel2]["ingredients"][x]     
     return
 
 #funcion para que solo se entren numeros
-def check_number(coin):
+def input_int(msg):
     while True:                                                 #loop para asegurar que entre un numero
-        num=input(f'\nhow many {coin}?: ')
+        num=input(msg)
         try:
             num=float(num)
             break
-        except:
-            print('Valor invalido, introduzca un numero')
+        except: print('Valor invalido, introduzca un numero')
     return num
 
-def making(sel):   #TODO: desarrollar funcion para hacer el cafe
-    #proceso para introducir monedas
+ #funcion para hacer el cafe
+def making(sel):  
+    #proceso para introducir monedas  
     global money
     print("\nPlease insert coins.")
-    q=check_number("quaters")   #quaters
-    d=check_number("dimes")     #dimes
-    n=check_number("nickles")   #nickles
-    p=check_number("pennies")   #pennies
+    q=input_int("how many quaters? ")   #quaters
+    d=input_int("how many dimes? ")     #dimes
+    n=input_int("how many nickles? ")   #nickles
+    p=input_int("how many pennies? ")   #pennies
     total=q*0.25+d*0.10+n*0.05+p*0.01
 
-    
     if total>=MENU[sel]["cost"]:        #si el dinero es suficiente ofrece el servicio
         change=round(total-MENU[sel]["cost"],2)
         print(f"Here is {change} in change.")
@@ -49,35 +47,34 @@ def making(sel):   #TODO: desarrollar funcion para hacer el cafe
         money+=money+MENU[sel]["cost"]
         #update resources
         updateresouces(sel)
-
     else:                                #si el dinero no es suficiente 
-        print("Sorry that's not enough money. Money refunded.")
-    
+        print("Sorry that's not enough money. Money refunded.")  
     return
+
 #funcion principal
 def main():
-
     while True:                                             #loop para asegurar que seleccione y or n
-        selection=input('\n\nWhat would you like? (espresso/latte/cappuccino): ').lower()
-        if selection in options or selection=="report" or selection=="off": break
-        else: print("Option invalida")
+        selection=input('\nWhat would you like? (espresso/latte/cappuccino): ').lower()
+        if selection in options : break
+        else: print("invalid Option")
 
-    if selection in options:       #se procesa el cafe seleccionado
-        if check_resources(selection):
+    if selection in options[0:2]:       #se procesa el cafe seleccionado
+        if check_resources(selection):  #para saber si hay con que prepararlo
             making(selection)
-        else: print("Sorry there is not enough water.")
+        else: print("Sorry there is not enough resources.")
        
     elif selection=="report":   #se imprime el reporte
         print(f"""
     water     : {resources["water"]} ml
     milk      : {resources["milk"]} ml
     coffee    : {resources["coffee"]} mg 
-    Money     : {money} $                      """)
+    Money     : ${money}                       """)
     else:                       #se selecciono off
         return "off"
 
+os.system('cls')
+print(Logo)
 while True:
- if main()=="off":
-     break
-
+    if main()=="off":
+        break
 print("Thank you")
